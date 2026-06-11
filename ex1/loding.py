405a1c016820a357dec44fb8a38ebcbb8b27bdd5
Youssef Kaf <ykaf@c1r1p8.1337.ma>
2026-06-11 18:07:35 +0100

    finished

--- ex1/loding.py
+++ ex1/loding.py
@@ -2,31 +2,43 @@ import importlib
 
 
 def main() -> None:
+    try:
 
-    print("\nLOADING STATUS: Loading programs...\n")
+        print("\nLOADING STATUS: Loading programs...\n")
 
-    packages = {
-        "pandas": "Data manipulation ready",
-        "numpy": "Numerical computation ready",
-        "matplotlib": "Visualization ready",
-        "requests": "Network access ready"
-    }
+        packages = {
+            "pandas": "Data manipulation ready",
+            "numpy": "Numerical computation ready",
+            "matplotlib": "Visualization ready",
+            "requests": "Network access ready"
+        }
 
-    for package, descreption in packages.items():
-        try:
-            module = importlib.import_module(package)
-            print(f"[OK] {package} ({module.__version__}) - {descreption}")
-        except ImportError:
-            print(f"[Error] {package} not installed")
-            print("use this:")
-            print("pip install -r requirements.txt")
-            print("or use:")
-            print("poetry install")
-            print("poetry run python loading.py")
+        modules = {}
 
-    print("Analyzing Matrix data...")
-    print("Processing 1000 data points...")
-    print("Generating visualization...")
+        for package, descreption in packages.items():
+            try:
+                module = importlib.import_module(package)
+                modules[package] = module
+                print(f"[OK] {package} ({module.__version__}) - {descreption}")
+            except ImportError:
+                print(f"[Error] {package} not installed")
+                print("use this:")
+                print("pip install -r requirements.txt")
+                print("or use:")
+                print("poetry install")
+                print("poetry run python loading.py")
+
+        print("Analyzing Matrix data...")
+        random_nums = modules["numpy"].random.randint(1, 101, size=1000)
+
+        print("Processing 1000 data points...")
+        md = modules["pandas"].DataFrame(
+            random_nums, columns=['Matrix_Data'])
+
+        print("Generating visualization...")
+
+    except KeyError as e:
+        print(e)
 
 
 if __name__ == "__main__":
