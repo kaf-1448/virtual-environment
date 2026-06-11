d252c286a3ff2363084bc331c0a3d62c1e319b6d
Youssef Kaf <ykaf@c1r1p8.1337.ma>
2026-06-11 16:09:42 +0100

    finished

--- ex1/loding.py
+++ ex1/loding.py
@@ -2,19 +2,31 @@ import importlib
 
 
 def main() -> None:
+
+    print("\nLOADING STATUS: Loading programs...\n")
+
     packages = {
         "pandas": "Data manipulation ready",
         "numpy": "Numerical computation ready",
-        "matplotlib": "Network access ready",
-        "requests": "Visualization ready"
+        "matplotlib": "Visualization ready",
+        "requests": "Network access ready"
     }
 
     for package, descreption in packages.items():
         try:
             module = importlib.import_module(package)
             print(f"[OK] {package} ({module.__version__}) - {descreption}")
-        except (ImportError, ModuleNotFoundError):
-            print(f"[Error] {package} not dowload")
+        except ImportError:
+            print(f"[Error] {package} not installed")
+            print("use this:")
+            print("pip install -r requirements.txt")
+            print("or use:")
+            print("poetry install")
+            print("poetry run python loading.py")
+
+    print("Analyzing Matrix data...")
+    print("Processing 1000 data points...")
+    print("Generating visualization...")
 
 
 if __name__ == "__main__":
